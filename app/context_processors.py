dbbb4e1651831b0f20fabc068d58ada7b3ab5793
Tinashe Matanda <tinashelorenzi@protonmail.com>
2025-02-18 08:49:10 +0200

    Staging the property listing page

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 def constants(request):
     # Get user data from session
     user_data = request.session.get('userData', None)
@@ -8,6 +13,7 @@ def constants(request):
         'SUPPORT_EMAIL': 'support@example.com',
         'USERDATA': user_data,
         'TOKEN': request.session.get('access_token', None),
+        'DEPLOYMENT_MODE': os.getenv('DEPLOYMENT_MODE'),
     }
     
     # Add agency data only if user is an Agency Admin
